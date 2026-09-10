4aea1328fcc46cc01e9bba9c14badadb45b6f7e1
Dev khatri <devkhatri@rezs-Mac-mini.local>
2026-09-11 00:24:40 +0530

    redis

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const createclient = require('redis').createClient;
 
 
 const app = express();
 app.use(express.json());
+const redis = createclient({
+    url: 'redis://127.0.0.1:6379'
+});
+
+redis.on('error', (err) => console.log('Redis Client Error', err));
+
 
 mongoose.connect('mongodb://localhost:27017/')
 .then(() => {console.log('connected to database')})
@@ -74,27 +81,57 @@ app.get('/users', async (req, res) => {
         !username ? res.status(400).json({message: 'username is required'}) : res.status(400).json({message: 'email is required'});
     }
 
+    const rediskey = username
+
+    ? `user:username:${username}`
+    : `email:${email}`;
+
+    const cachedUser = await redis.get(rediskey);
+
+    if(cachedUser){
+
+        console.log('User Found In Redis');
+        return res.status(200).json(JSON.parse(cachedUser));
+    }
+
+    console.log('User Not Find In Redis, Checking MongoDB');
+
+
+
     const user = await User.findOne({$or: [{username}, {email}]})
 
     if(!user) {
         return res.status(404).json({message: 'user not found'});
     }
 
-    return res.status(200).json({
-        id: user._id,
+
+    const userData = {
         username: user.username,
         email: user.email,
         password: user.password
-    })
+    };
+
+    await redis.set(rediskey, JSON.stringify(userData), {
+        EX: 300
+    });
+
+    console.log('User Found In MongoDB, Saving To Redis');
+
+    return res.status(200).json(userData);
+
 });
-    
 
 
 
-
+redis.connect().then(() => {
+    console.log('connected to redis');
+}).catch((err) => {
+    console.log('error in connecting to redis', err);
+}); 
 
 
 app.listen(3000, () => {
     console.log('server is running http://localhost:3000');
 });
+
 
